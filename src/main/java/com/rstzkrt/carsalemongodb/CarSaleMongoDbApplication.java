d1d5bb2b79df8a64e1dc5641907f1a6c76c4ed7d
resat <ozkurtresat@gmail.com>
2021-09-05 22:32:17 +0200

    update directory path

--- src/main/java/com/rstzkrt/carsalemongodb/CarSaleMongoDbApplication.java
+++ src/main/java/com/rstzkrt/carsalemongodb/CarSaleMongoDbApplication.java
@@ -1,4 +1,4 @@
-package com.example.carsalemongodb;
+package com.rstzkrt.carsalemongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/rstzkrt/carsalemongodb/CarSaleMongoDbApplicationTests.java
+++ src/test/java/com/rstzkrt/carsalemongodb/CarSaleMongoDbApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.carsalemongodb;
+package com.rstzkrt.carsalemongodb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
