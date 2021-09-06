681e8ffdc52da4acd1dd07ffd3dce6f73beda04a
resat <ozkurtresat@gmail.com>
2021-09-06 12:23:11 +0200

    add findAllByPostedBy method
    i added this method to find all adverts whose posted by

--- src/main/java/com/rstzkrt/carsalemongodb/Advert/AdvertRepository.java
+++ src/main/java/com/rstzkrt/carsalemongodb/Advert/AdvertRepository.java
@@ -1,9 +1,13 @@
 package com.rstzkrt.carsalemongodb.Advert;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "adverts", path = "adverts")
 public interface AdvertRepository extends MongoRepository<Advert,String> {
 
+    List<Advert> findAllByPostedBy(ObjectId postedBy);
 }
