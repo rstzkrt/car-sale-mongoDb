bbffb712b82d2f5f9aa37e9ec16c2b4547206226
resat <ozkurtresat@gmail.com>
2021-09-06 12:23:55 +0200

    add RepositoryRestController annotation

--- src/main/java/com/rstzkrt/carsalemongodb/Advert/AdvertController.java
+++ src/main/java/com/rstzkrt/carsalemongodb/Advert/AdvertController.java
@@ -1,5 +1,10 @@
 package com.rstzkrt.carsalemongodb.Advert;
 
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RepositoryRestController
 public class AdvertController {
+
 }
