a0116e773b57cd2b8473c847ba3e27a017741e98
resat <ozkurtresat@gmail.com>
2021-09-06 18:13:03 +0200

    override some data rest end points
    i implemented my own response DTO to retrieve data easily on the front side

--- src/main/java/com/rstzkrt/carsalemongodb/Advert/AdvertController.java
+++ src/main/java/com/rstzkrt/carsalemongodb/Advert/AdvertController.java
@@ -1,10 +1,34 @@
 package com.rstzkrt.carsalemongodb.Advert;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@CrossOrigin
+import java.util.List;
+
 @RepositoryRestController
 public class AdvertController {
 
+    @Autowired
+    private AdvertService advertService;
+
+    @GetMapping("adverts")
+    public ResponseEntity<Object> getAdverts() {
+        List<AdvertResponseDTO>advertResponseDTOList =advertService.getAdverts();
+        return ResponseEntity.status(200).body(advertResponseDTOList);
+    }
+
+    @GetMapping("adverts/{advertId}")
+    public ResponseEntity<Object> getAdvert(@PathVariable("advertId") String advertId) {
+        AdvertResponseDTO advertResponseDTO=advertService.getAdvert(advertId);
+        return ResponseEntity.status(200).body(advertResponseDTO);
+    }
+
+    @GetMapping("adverts/findByBrand/{brand}")
+    public ResponseEntity<Object> getAdvertByBrand(@PathVariable("brand") String brand) {
+        List<AdvertResponseDTO>advertResponseDTOList =advertService.getAdvertsByBrand(brand);
+        return ResponseEntity.status(200).body(advertResponseDTOList);
+    }
 }
