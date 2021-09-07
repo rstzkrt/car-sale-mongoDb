2b1f6663759396776cab3314c026e067ded7187f
resat <ozkurtresat@gmail.com>
2021-09-07 11:43:52 +0200

    update Advert save and remove methods

--- src/main/java/com/rstzkrt/carsalemongodb/Advert/AdvertService.java
+++ src/main/java/com/rstzkrt/carsalemongodb/Advert/AdvertService.java
@@ -1,6 +1,9 @@
 package com.rstzkrt.carsalemongodb.Advert;
 
 
+import com.rstzkrt.carsalemongodb.Advert.elasticSearch.EsAdvert;
+import org.springframework.data.elasticsearch.core.SearchHit;
+
 import java.util.List;
 
 public interface AdvertService {
@@ -10,4 +13,11 @@ public interface AdvertService {
     List<AdvertResponseDTO> getAdverts();
 
     List<AdvertResponseDTO> getAdvertsByBrand(String brand);
+
+
+    Advert saveOrUpdateAdvert(Advert advert);
+
+    void removeAdvert(String id);
+
+    List<AdvertResponseDTO> searchHitsToAdvertDTO(List<SearchHit<EsAdvert>> searchHits);
 }
