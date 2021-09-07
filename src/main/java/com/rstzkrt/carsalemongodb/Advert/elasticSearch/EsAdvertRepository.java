4f842da2aed3e05f2c44c107d2927113cae19c51
resat <ozkurtresat@gmail.com>
2021-09-07 11:42:11 +0200

    update elsatricSearch query
    search all adverts by description title city and carBrand

--- src/main/java/com/rstzkrt/carsalemongodb/Advert/elasticSearch/EsAdvertRepository.java
+++ src/main/java/com/rstzkrt/carsalemongodb/Advert/elasticSearch/EsAdvertRepository.java
@@ -1,2 +1,20 @@
-package com.rstzkrt.carsalemongodb.Advert.elasticSearch;public class EsAdvertRepository {
+package com.rstzkrt.carsalemongodb.Advert.elasticSearch;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface EsAdvertRepository extends ElasticsearchRepository<EsAdvert, String> {
+
+
+
+    //@Query("{\"bool\": {\"must\": [{\"match\": {\"title\": \"?0\"}}]}}")
+    @Query("{\"multi_match\": {\"query\": \"?0\",\"fields\": [\"title^1.0\",\"description^1.0\" , \"city^1.0\",\"carBrand^1.0\"],\"type\": \"phrase_prefix\",\"operator\": \"OR\",\"slop\": 0,\"prefix_length\": 0,\"max_expansions\": 50,\"zero_terms_query\": \"NONE\",\"auto_generate_synonyms_phrase_query\": true,\"fuzzy_transpositions\": true,\"boost\": 1}}")
+    List<SearchHit<EsAdvert>> queryDeals(String keyword, Pageable pageable);
+
+    void deleteEsAdvertById(String id);
+
 }
