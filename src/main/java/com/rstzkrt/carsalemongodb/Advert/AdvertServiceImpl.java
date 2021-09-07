9a987e43c0c73a08dff502a0b7048240776da72f
resat <ozkurtresat@gmail.com>
2021-09-07 11:45:00 +0200

    update saveOrUpdate and remove methods

--- src/main/java/com/rstzkrt/carsalemongodb/Advert/AdvertServiceImpl.java
+++ src/main/java/com/rstzkrt/carsalemongodb/Advert/AdvertServiceImpl.java
@@ -1,9 +1,15 @@
 package com.rstzkrt.carsalemongodb.Advert;
 
+import com.rstzkrt.carsalemongodb.Advert.elasticSearch.EsAdvert;
+import com.rstzkrt.carsalemongodb.Advert.elasticSearch.EsAdvertRepository;
+import com.rstzkrt.carsalemongodb.Advert.elasticSearch.EsAdvertService;
 import com.rstzkrt.carsalemongodb.User.User;
 import com.rstzkrt.carsalemongodb.User.UserRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +23,9 @@ public class AdvertServiceImpl implements AdvertService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private EsAdvertRepository esAdvertRepository;
 
     @Override
     public AdvertResponseDTO getAdvert(String advertId) {
@@ -63,6 +72,47 @@ public class AdvertServiceImpl implements AdvertService {
                     advert.getCarFuelType(), advert.getCarCondition(), advert.getPhotos(), user);
 
             advertResponseDTOList.add(advertResponseDTO);
+        }
+        return advertResponseDTOList;
+    }
+
+    @Transactional
+    @Override
+    public Advert saveOrUpdateAdvert(Advert advert) {
+        Advert tempAdvert = advertRepository.save(advert);
+        User user=userRepository.findById(String.valueOf(advert.getPostedBy())).orElse(null);
+        AdvertResponseDTO responseDTO=new AdvertResponseDTO(
+                advert.getId(), advert.getPostedBy(), advert.getDescription(), advert.getTitle(),
+                advert.getPrice(), advert.getPostDate(), advert.getCity(), advert.getCoverPhoto(),
+                advert.getCarBrand(), advert.getCarTransmission(), advert.getCarMileage(), advert.getCarBodyType(),
+                advert.getCarFuelType(), advert.getCarCondition(), advert.getPhotos(), user);
+
+        esAdvertRepository.save(new EsAdvert(responseDTO));
+        return tempAdvert;
+    }
+
+    @Transactional
+    @Override
+    public void removeAdvert(String id) {
+        advertRepository.deleteById(id);
+        esAdvertRepository.deleteEsAdvertById(id);
+    }
+
+    @Override
+    public List<AdvertResponseDTO> searchHitsToAdvertDTO(List<SearchHit<EsAdvert>> searchHits) {
+        List<AdvertResponseDTO> advertResponseDTOList=new ArrayList<>();
+        for(SearchHit<EsAdvert> esAdvertSearchHit:searchHits){
+            ObjectId userid=esAdvertSearchHit.getContent().getPostedById();
+            User user=userRepository.findById(String.valueOf(userid)).orElse(null);
+            Advert advert=advertRepository.findById(esAdvertSearchHit.getContent().getId()).orElse(null);
+
+            AdvertResponseDTO responseDTO=new AdvertResponseDTO(
+                    advert.getId(), advert.getPostedBy(), advert.getDescription(), advert.getTitle(),
+                    advert.getPrice(), advert.getPostDate(), advert.getCity(), advert.getCoverPhoto(),
+                    advert.getCarBrand(), advert.getCarTransmission(), advert.getCarMileage(), advert.getCarBodyType(),
+                    advert.getCarFuelType(), advert.getCarCondition(), advert.getPhotos(), user);
+
+            advertResponseDTOList.add(responseDTO);
         }
         return advertResponseDTOList;
     }
