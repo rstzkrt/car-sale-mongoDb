4d7e067b16e647834f10a8b14e1418b78db140bd
resat <ozkurtresat@gmail.com>
2021-09-07 11:44:15 +0200

    add ESAdvertService

--- src/main/java/com/rstzkrt/carsalemongodb/Advert/elasticSearch/EsAdvertService.java
+++ src/main/java/com/rstzkrt/carsalemongodb/Advert/elasticSearch/EsAdvertService.java
@@ -1,2 +1,18 @@
-package com.rstzkrt.carsalemongodb.Advert.elasticSearch;public interface EsAdvertService {
+package com.rstzkrt.carsalemongodb.Advert.elasticSearch;
+
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.SearchHit;
+
+
+public interface EsAdvertService {
+
+    EsAdvert saveOrUpdate(EsAdvert esADvert);
+
+    void deleteByAdvertId(String id);
+
+    Page<EsAdvert> findAll(Pageable pageable);
+
+    List<SearchHit<EsAdvert>> queryAdverts(String keyword, Pageable pageable);
 }
