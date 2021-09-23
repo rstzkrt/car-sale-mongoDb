a620c7fc9bd1ad0ea7ab90c1a546f95c85da2af9
resat <ozkurtresat@gmail.com>
2021-09-23 17:04:52 +0200

    add uid for firbase authentication

--- src/main/java/com/rstzkrt/carsalemongodb/User/User.java
+++ src/main/java/com/rstzkrt/carsalemongodb/User/User.java
@@ -1,25 +1,17 @@
 package com.rstzkrt.carsalemongodb.User;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.rstzkrt.carsalemongodb.Advert.Advert;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
 import org.hibernate.validator.constraints.URL;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Document(collection = "users")
@@ -32,9 +24,9 @@ public class User {
     @Id
     private String id;
 
-//    @Indexed(unique = true)
-//    @NotBlank
-//    private String uid;
+    @Indexed(unique = true)
+    @NotBlank
+    private String uid;
 
     private String firstName;
 
@@ -43,15 +35,18 @@ public class User {
     @Email
     private String email;
 
-    private String role;
-
     @URL
     private String avatar;
 
-    @DateTimeFormat
-    private Instant dateOfBirth;
-
     private Map<String, Boolean> favorites = new HashMap<>();
 
-    private List<Advert> myAdverts=new ArrayList<>();
+    public User(String id) {
+        this.id = id;
+    }
+
+    public User(String uid, String email, String avatar) {
+        this.uid = uid;
+        this.email = email;
+        this.avatar = avatar;
+    }
 }
