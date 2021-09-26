6be156def0c7e5d11cd6d1299c804e7bda095346
resat <ozkurtresat@gmail.com>
2021-09-26 11:47:49 +0200

    add test cases for Advert

---
+++ src/test/java/com/rstzkrt/carsalemongodb/Advert/AdvertControllerTest.java
@@ -0,0 +1,32 @@
+package com.rstzkrt.carsalemongodb.Advert;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdvertControllerTest {
+
+    @Test
+    void getAdverts() {
+    }
+
+    @Test
+    void getAdvert() {
+    }
+
+    @Test
+    void getAdvertByBrand() {
+    }
+
+    @Test
+    void searchDeals() {
+    }
+
+    @Test
+    void saveOrUpdateDeal() {
+    }
+
+    @Test
+    void removeDeal() {
+    }
+}
---
+++ src/test/java/com/rstzkrt/carsalemongodb/Advert/AdvertServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.rstzkrt.carsalemongodb.Advert;
+
+import org.junit.jupiter.api.Test;
+
+
+class AdvertServiceImplTest {
+
+
+    @Test
+    void getAdvert() {
+    }
+
+    @Test
+    void getAdverts() {
+    }
+
+    @Test
+    void getAdvertsByBrand() {
+    }
+
+    @Test
+    void saveOrUpdateAdvert() {
+    }
+
+    @Test
+    void removeAdvert() {
+    }
+
+    @Test
+    void searchHitsToAdvertDTO() {
+    }
+}
